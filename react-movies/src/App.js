9461894bffd325eeaf0e5c469c4e8ebd223d5c2c
rachid <olgor@hotmail.com>
2021-02-15 21:41:28 +0100

    src ans pudlic added

---
+++ react-movies/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+      <Switch>
+      
+      </Switch>
+      </div>
+    </BrowserRouter>
+    );
+    
+}
+
+export default App;
